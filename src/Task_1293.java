c49d0d546c29fdb3213c3f7db5eea263c0a26df0
TreVogas <63095431+TreVogas@users.noreply.github.com>
2021-05-28 11:11:57 +0500

    Timus_task

---
+++ src/Task_1293.java
@@ -0,0 +1,9 @@
+//'задача - 1293
+import java.util.*;
+import java.util.Scanner;
+public class Task_1293 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(in.nextInt() * 2 * in.nextInt() * in.nextInt());
+    }
+}
